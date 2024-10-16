9aff63bf83699871cbd4f232c3a012f0d48660c0
Vita Tri Utami <110320983+vitatriutami@users.noreply.github.com>
2024-10-16 17:42:35 +0700

    fix: fix UserHandler on Infrastructures

--- src/Interfaces/http/api/users/handler.js
+++ src/Interfaces/http/api/users/handler.js
@@ -1,6 +1,4 @@
 const AddUserUseCase = require('../../../../Applications/use_case/AddUserUseCase');
-const ClientError = require('../../../../Commons/exceptions/ClientError');
-const DomainErrorTranslator = require('../../../../Commons/exceptions/DomainErrorTranslator');
 
 class UsersHandler {
   constructor(container) {
@@ -10,35 +8,16 @@ class UsersHandler {
   }
 
   async postUserHandler(request, h) {
-    try {
-      const addUserUseCase = this._container.getInstance(AddUserUseCase.name);
-      const addedUser = await addUserUseCase.execute(request.payload);
-
-      const response = h.response({
-        status: 'success',
-        data: {
-          addedUser,
-        },
-      });
-      response.code(201);
-      return response;
-    } catch (error) {
-      const translatedError = DomainErrorTranslator.translate(error);
-      if (translatedError instanceof ClientError) {
-        const response = h.response({
-          status: 'fail',
-          message: translatedError.message,
-        });
-        response.code(translatedError.statusCode);
-        return response;
-      }
-      const response = h.response({
-        status: 'error',
-        message: 'terjadi kegagalan pada server kami',
-      });
-      response.code(500);
-      return response;
-    }
+    const addUserUseCase = this._container.getInstance(AddUserUseCase.name);
+    const addedUser = await addUserUseCase.execute(request.payload);
+    const response = h.response({
+      status: 'success',
+      data: {
+        addedUser,
+      },
+    });
+    response.code(201);
+    return response;
   }
 }
 
